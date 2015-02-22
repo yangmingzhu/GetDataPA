dd3ecf7a55e70d3b8ff73f565482a7d5f1a1000a
Yang-Ming Zhu <yzhu@computer.org>
2015-02-22 14:36:18 -0500

    first attemp

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # GetDataPA
 Peer assessment project for "getting and cleaning data"
+
+The detailed steps how to fulfill the project requirements are described below.
+
+## Merges the training and the test sets to create one data set.
+
+Three functions are used for that purpose: merge_subject(), merge_X(), and merge_y(). The folders in both train and test folders are ignored. All three functions work in the same sashion: the txt files are read from test and train folders and the data frames are combined via rbind. The merged data frame is simply returned. 
+
+## Extracts only the measurements on the mean and standard deviation for each measuremen
+
+The features table is loaded first. The feature descriptions are analyzed carefully. We decided to extract all features that involve the mean or standard deviation. The rows can be grep'ed where the match string is mean, Mean, or std. The row indexes returned by grep are used to subset the columns in X. To make further processing easy, the features table is also subset using the same row indexes.
+
+## Uses descriptive activity names to name the activities in the data set
+
+The activity labels are loaded. Note that the activity is coded in the y table. One would merge (join) the two tables, which would change the order. If not processed carefully, a wrong activity would be assigned to the rows in X. To work around that, a function, fix_activity(), is coded, where we simply loop through the y table and use the LUT (look up table) to replace the activity using the activity label.
+
+## Appropriately labels the data set with descriptive variable names
+
+The column names of the subset X are replaced with the subset feature descriptions. As those feature descriptions convey the meaning of the variable using a succinct "math" notation, we think they are appropriate, precise and concise.
+
+We put all data together, the subject and activity information is merged with the subset X, with appropriate column names for subject and activity as well.
+
+## Creates a second, independent tidy data set with the average of each variable for each activity and each subject
+
+TBD
---
+++ run_analysis.R
@@ -0,0 +1,79 @@
+
+
+process_data <- function() {
+  # pt1: Merges the training and the test sets to create one data set.
+  # merge subject, X, and y data from train and test folders
+  # ignore Inertial Signals
+  subject <- merge_subject()
+  X <- merge_X()
+  y <- merge_y()
+  
+  
+  # pt2: Extracts only the measurements on the mean and standard deviation for each measurement.
+  # load feature vectors
+  features <- read.table("./features.txt")
+  # filter the features with mean/std in them
+  sub_feature_idx <- grep("std|mean|Mean", features$V2)
+  # subset X by the column index
+  subX <- X[, sub_feature_idx]
+  
+  # subset features as well
+  sub_features <- features[sub_feature_idx, ]
+  
+  
+  # pt3: Uses descriptive activity names to name the activities in the data set
+  # load activity labels
+  activity_labels <- read.table("./activity_labels.txt") 
+  # merge changes the order, thus we do that way
+  activity <- fix_activity(y, activity_labels)
+
+  
+  # pt4: Appropriately labels the data set with descriptive variable names 
+  # use those simple definitions as the meaningful var names
+  names(subX) = sub_features$V2
+  # change the column name of the subject
+  colnames(subject) <- "subject"
+  
+  # combine all data together
+  data <- cbind(subject, activity, subX)
+  
+  
+}
+
+fix_activity <- function(y, activity_labels) {
+  activity = character(length=nrow(y))
+  for (i in 1:nrow(y)) {
+    for (j in 1:nrow(activity_labels))
+    {
+      if (y$V1[i]==activity_labels$V1[j]) {
+        activity[i] = as.character(activity_labels$V2[j])
+        break
+      }      
+    }
+  }
+  as.factor(activity)
+}
+
+merge_subject <- function() {
+  # load subject data from train and test folders and combine them
+  subject_train <- read.table("./train/subject_train.txt")
+  subject_test <- read.table("./test/subject_test.txt")
+  subject <- rbind(subject_train, subject_test)
+  subject
+}
+
+merge_X <- function() {
+  # load X data from train and test folders and combine them
+  X_train <- read.table("./train/X_train.txt")
+  X_test <- read.table("./test/X_test.txt")
+  X <- rbind(X_train, X_test)
+  X
+}
+
+merge_y <- function() {
+  # load y data from train and test folders and combine them
+  y_train <- read.table("./train/y_train.txt")
+  y_test <- read.table("./test/y_test.txt")
+  y <- rbind(y_train, y_test)
+  y
+}
